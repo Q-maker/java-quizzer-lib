412382b03a3e0921ccd82a9d698e1c3facf6b4ee
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2019-11-23 14:48:59 +0000

    les qcm a qui~zzer sont toujours randomisé

--- src/main/java/com/qmaker/quizzer/core/entities/Quiz.java
+++ src/main/java/com/qmaker/quizzer/core/entities/Quiz.java
@@ -243,6 +243,7 @@ public class Quiz implements QPackage {
         maxAnswerTrue = bundle.getInt(Quiz.FIELD_MAX_TRUE_ANSWER_COUNT_PER_EXERCISE, 1);
 
         for (Qcm qcm : qcms.getQcms()) {
+            qcm.setPropositionRandomizationType(Qcm.PROPOSITION_RANDOMIZATION_TYPE_ALWAYS);
             if (qcm.getMaxPropositionPerExercise() <= 0) {
                 qcm.setMaxPropositionPerExercise(maxAnswer);
             }
